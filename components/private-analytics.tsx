dd49d3096e5be9d4bce403e0dc46cb45100aa476
Diego Liñan <diegolinan@hotmail.com>
2026-09-14 01:06:29 -0300

    Use portable Vercel analytics integration

--- components/private-analytics.tsx
+++ components/private-analytics.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export function PrivateAnalytics() {
   if (process.env.NEXT_PUBLIC_ENABLE_VERCEL_ANALYTICS !== 'true') return null;
